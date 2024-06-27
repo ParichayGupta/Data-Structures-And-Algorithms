118ae55f00da9ab7ef7437124bd25094744791f9
parichaygupta <parichaygupta93@gmail.com>
2024-06-27 14:35:48 +0530

    Factors of a number

---
+++ src/Factors.java
@@ -0,0 +1,20 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+
+    public static void main(String[] args) {
+        System.out.println(factors(100));
+    }
+
+    private static List<Integer> factors(int n){
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 1; i <= n/i; i++){
+            if(n % i == 0) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
